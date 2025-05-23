29a5327254b052c7ca4362231d7c7265c398ef6a
Natasha <salonidhingra170@gmail.com>
2025-05-24 00:41:53 +0530

    After Deleting env

--- backend/settings.py
+++ backend/settings.py
@@ -12,7 +12,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--rb!mp7jj*db%4hnk%kwg0w(_5hlv%_a0no7_&vry7)z&-vco&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
