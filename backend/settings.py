770a6e90f0651d38b1b80c9851c6a5dd8b333ddc
Saloni <89639939+Saloni1406@users.noreply.github.com>
2025-05-26 15:13:00 +0000

    Remove HardCoded security key

--- backend/settings.py
+++ backend/settings.py
@@ -3,7 +3,6 @@ import os
 from decouple import config
 
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -12,13 +11,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--rb!mp7jj*db%4hnk%kwg0w(_5hlv%_a0no7_&vry7)z&-vco&'
+
+SECRET_KEY = config('DJANGO_SECRET_KEY', default='fallback-insecure-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["dr-neeraj.onrender.com"]
-
+#ALLOWED_HOSTS = ["dr-neeraj.onrender.com"]
+ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -109,14 +109,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,"static")
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
